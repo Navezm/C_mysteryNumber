ea71e228a24f8847867fc12dc658324ad1c9516e
Navezm <navez.martin@gmail.com>
2021-02-16 15:05:43 +0100

    Project done

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    const int MAX = 100, MIN = 1;
+    int mysteryNumber;
+    int answer;
+
+    srand(time(NULL)); // Initiate a random number generator 
+    mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN; // Get the random value
+
+    printf("Welcome into my first game. \n");
+    printf("You'll have to find a random number between 1 and 100\n");
+    
+    do
+    {
+        printf("What's the number ?");
+        scanf("%d", &answer);
+        if (mysteryNumber != answer)
+        {
+            if (mysteryNumber < answer)
+            {
+                printf("\nIt's less !\n");
+            }
+            else
+            {
+                printf("\nIt's more !\n");
+            }
+        }
+        
+    } while (mysteryNumber != answer);
+    printf("Well done you found the right number !\n\n");
+
+
+    // printf("%d", mysteryNumber);
+
     return 0;
 }
